013a3787453aa35f90bf9d89731e54f64c4a6d31
Joseph Dubon <josephdubon@pm.me>
2020-09-02 14:03:29 -0700

    Added labels to home screen to contrast login/sign up options.

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -9,7 +9,10 @@ export const HomeScreen = () => (
   <>
     <MenuContainer />
     <h2>Your favorite microblogging platform</h2>
+    <h3>Login</h3>
     <LoginFormContainer />
+    <br />
+    <h3>Sign Up</h3>
     <SignUpFormContainer />
   </>
 );
