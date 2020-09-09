27bd51010a9ae82cf5c12d056fc58a3dc1254219
Joseph Dubon <josephdubon@pm.me>
2020-09-08 20:26:40 -0700

    Applied RS styles and css to navbar.

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -5,26 +5,34 @@ import { actions } from "../../redux/actions/";
 
 import "./Menu.css";
 
+import { Navbar } from "rsuite";
+
 export const Menu = () => {
   const isAuthenticated = useSelector((state) => !!state.auth.isAuthenticated);
   const dispatch = useDispatch();
   const logout = () => dispatch(actions.logout());
   return (
-    <div id="menu">
-      <Link to="/">
-        <h1>Kwitter</h1>
-      </Link>
-      <div id="menu-links">
-        {isAuthenticated ? (
-          <>
-            <Link to="/messagefeed">Message Feed</Link>
-            <Link to ="/users">Users</Link>
-            <Link to="/" onClick={logout}>
-              Logout
-            </Link>
-          </>
-        ) : null}
+    <Navbar appearance="inverse" componentClass="div">
+      <div id="menu">
+        <Navbar.Header>
+          <Link to="/">
+            <h2>KWITTER</h2>
+          </Link>
+        </Navbar.Header>
+        <div id="menu-links">
+          {isAuthenticated ? (
+            <>
+              <Navbar.Body>
+                <Link to="/messagefeed">Message Feed</Link>
+                <Link to="/users">Users</Link>
+                <Link to="/" onClick={logout}>
+                  Logout
+                </Link>
+              </Navbar.Body>
+            </>
+          ) : null}
+        </div>
       </div>
-    </div>
+    </Navbar>
   );
 };
