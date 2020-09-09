b67f2e06d94fdcce357d7537779d030492e4d60b
Joseph Dubon <josephdubon@pm.me>
2020-09-09 13:45:27 -0700

    Completed messages component. Tested and ready to party.

--- src/redux/actions/messages.js
+++ src/redux/actions/messages.js
@@ -1,22 +1,18 @@
 import api from "../../utils/api";
 
 export const MESSAGES = "MESSAGES";
-export const MESSAGE_FEED_SUCCESS = "MESSAGE_FEED_SUCCESS";
-export const MESSAGE_FEED_FAILURE = "MESSAGE_FEED_FAILURE";
+export const MESSAGES_SUCCESS = "MESSAGES_SUCCESS";
+export const MESSAGES_FAILURE = "MESSAGES_FAILURE";
 
 export const messages = () => async (dispatch, getState) => {
   try {
     dispatch({ type: MESSAGES });
     const payload = await api.messages();
-    dispatch({ type: MESSAGE_FEED_SUCCESS, payload });
+    dispatch({ type: MESSAGES_SUCCESS, payload });
   } catch (err) {
     dispatch({
-      type: MESSAGE_FEED_FAILURE,
+      type: MESSAGES_FAILURE,
       payload: err.message,
     });
   }
-};
-
-export const actions = {
-  messages,
 };
