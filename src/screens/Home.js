dd6e3e2358a315a80af06db13bb1def76c2e0bc0
Joseph Dubon <josephdubon@pm.me>
2020-09-08 21:06:51 -0700

    Implemented react-tabs for login and sign up options. Tested and 100% functional.

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -5,14 +5,29 @@ import {
   MenuContainer,
 } from "../components";
 
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+
 export const HomeScreen = () => (
   <>
     <MenuContainer />
-    <h2>Your favorite microblogging platform</h2>
-    <h3>Login</h3>
-    <LoginFormContainer />
-    <br />
-    <h3>Sign Up</h3>
-    <SignUpFormContainer />
+    <h4>Your favorite microblogging platform</h4>
+
+    <Tabs defaultFocus={true} defaultIndex={1}>
+      <TabList>
+        <Tab>Login</Tab>
+        <Tab>Sign Up</Tab>
+      </TabList>
+
+      <TabPanel>
+        <h3>Login</h3>
+        <LoginFormContainer />
+        <br />
+      </TabPanel>
+      <TabPanel>
+        <h3>Sign Up</h3>
+        <SignUpFormContainer />
+      </TabPanel>
+    </Tabs>
   </>
 );
