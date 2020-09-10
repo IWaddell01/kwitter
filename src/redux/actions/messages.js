2c0fa16ff4ec9ea426d3719013c5a08f24d2505f
Joseph Dubon <josephdubon@pm.me>
2020-09-09 17:28:26 -0700

    Created createMessages api end point, action, and reducer. Need to create component folder.

--- src/redux/actions/messages.js
+++ src/redux/actions/messages.js
@@ -1,9 +1,16 @@
 import api from "../../utils/api";
 
+// AUTH CONSTANTS MESSAGES
 export const MESSAGES = "MESSAGES";
 export const MESSAGES_SUCCESS = "MESSAGES_SUCCESS";
 export const MESSAGES_FAILURE = "MESSAGES_FAILURE";
 
+// AUTH CONSTANTS CREATE MESSAGE
+export const CREATE_MESSAGE = "CREATE_MESSAGE";
+export const CREATE_MESSAGE_SUCCESS = "CREATE_MESSAGE_SUCCESS";
+export const CREATE_MESSAGE_FAILURE = "CREATE_MESSAGE_FAILURE";
+
+// Messages
 export const messages = () => async (dispatch, getState) => {
   try {
     dispatch({ type: MESSAGES });
@@ -12,6 +19,22 @@ export const messages = () => async (dispatch, getState) => {
   } catch (err) {
     dispatch({
       type: MESSAGES_FAILURE,
+      payload: err.message,
+    });
+  }
+};
+
+// Create Message
+export const createMessage = (text) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: CREATE_MESSAGE });
+    const payload = await api.createMessage(text);
+    // ℹ️ℹ️This is how you woud debug the response to a requestℹ️ℹ️
+    // console.log({ result })
+    dispatch({ type: CREATE_MESSAGE_SUCCESS, payload });
+  } catch (err) {
+    dispatch({
+      type: CREATE_MESSAGE_FAILURE,
       payload: err.message,
     });
   }
