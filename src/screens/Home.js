e92619eb834bcf4df5ecf0cec9501e718c2f3d00
Joseph Dubon <josephdubon@pm.me>
2020-09-10 17:42:04 -0700

    Globally implemented RSuite Container component library to all screens/pages.

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -4,7 +4,9 @@ import {
   SignUpFormContainer,
   MenuContainer,
 } from "../components";
-import { FooterScreen as Footer } from "./Footer";
+
+import { FooterScreen } from "./Footer";
+import { Container, Header, Content, Footer } from "rsuite";
 
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 
@@ -15,35 +17,42 @@ import { AiFillEdit } from "react-icons/ai";
 
 export const HomeScreen = () => (
   <>
-    <MenuContainer />
-    <h4>Your favorite microblogging platform</h4>
-
-    <Tabs defaultFocus={true} defaultIndex={0}>
-      <TabList>
-        <Tab>
-          <AiOutlineLogin />
-          Login
-        </Tab>
-        <Tab>
-          <AiFillEdit />
-          Sign Up
-        </Tab>
-      </TabList>
-      <TabPanel>
-        <em>
-          <strong>Please enter details to login an acccount.</strong>
-        </em>
-        <br /> <br />
-        <LoginFormContainer />
-      </TabPanel>
-      <TabPanel>
-        <em>
-          <strong>Please enter details to create an acccount.</strong>
-        </em>
-        <br /> <br />
-        <SignUpFormContainer />
-      </TabPanel>
-    </Tabs>
-    <Footer />
+    <Container>
+      <Header>
+        <MenuContainer />
+        <h4>Your favorite microblogging platform</h4>
+      </Header>
+      <Content>
+        <Tabs defaultFocus={true} defaultIndex={0}>
+          <TabList>
+            <Tab>
+              <AiOutlineLogin />
+              Login
+            </Tab>
+            <Tab>
+              <AiFillEdit />
+              Sign Up
+            </Tab>
+          </TabList>
+          <TabPanel>
+            <em>
+              <strong>Please enter details to login an acccount.</strong>
+            </em>
+            <br /> <br />
+            <LoginFormContainer />
+          </TabPanel>
+          <TabPanel>
+            <em>
+              <strong>Please enter details to create an acccount.</strong>
+            </em>
+            <br /> <br />
+            <SignUpFormContainer />
+          </TabPanel>
+        </Tabs>
+      </Content>
+      <Footer>
+        <FooterScreen />
+      </Footer>
+    </Container>
   </>
 );
--- src/screens/NotFound.js
+++ src/screens/NotFound.js
@@ -1,10 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { MenuContainer } from "../components";
+
+import { FooterScreen } from "./Footer";
+import { Container, Header, Content, Footer } from "rsuite";
 
 const NotFound = ({ location }) => (
   <>
-    <p>Page not found for {location.pathname}</p>
-    <Link to="/">Go Home</Link>
+    <Container>
+      <Header>
+        <MenuContainer />
+        <h2>404</h2>
+      </Header>
+      <Content>
+        <p>Page not found for {location.pathname}</p>
+      </Content>
+      <Footer>
+        <FooterScreen />
+      </Footer>
+    </Container>
   </>
 );
 
