d63c90b675d75c346ba6b6f7ede994cbc125d2aa
Joseph Dubon <josephdubon@pm.me>
2020-09-11 15:46:44 -0700

    Fixed css loader.

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button } from "rsuite";
-import "./Loader.css"
+import "./Loader.css";
 
 /*
 https://lmgtfy.com/?q=react+loading+spinner+components
@@ -9,8 +9,6 @@ There are many more on the web use one of these or create your own!
 
 export const Loader = () => (
   <>
-    <Button className="loaderButton" appearance="ghost" loading>
-      Loading
-    </Button>
+    <Button className="loaderButton" appearance="ghost" loading></Button>
   </>
 );
