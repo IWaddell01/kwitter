ce534b7cf9b087ece445b0e5206a58bfbec6c855
Joseph Dubon <josephdubon@pm.me>
2020-09-12 18:34:12 -0700

    Added RSuite components to signup screen.

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,10 +1,22 @@
 import React from "react";
 import { SignUpFormContainer, MenuContainer } from "../components";
+import { FooterScreen } from "./Footer";
+
+import { Container, Header, Content, Footer } from "rsuite";
 
 export const SignUpScreen = () => (
   <>
-    <MenuContainer />
-    <h2>Sign up for Your favorite microblogging platform</h2>
-    <SignUpFormContainer />
+    <Container>
+      <Header>
+        <MenuContainer />
+      </Header>
+      <Content>
+        <h2>Sign up for Your favorite microblogging platform</h2>
+        <SignUpFormContainer />
+      </Content>
+      <Footer>
+        <FooterScreen />
+      </Footer>
+    </Container>
   </>
 );
